7bf594502689b99f92b41a89f8bcc5434c361611
Kelly Powers <kpowers.nj@gmail.com>
2020-01-22 01:47:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ end
   
 def line(katz_deli)
   if katz_deli.length == 0 
-    return "The line is currently empty"
+    puts "The line is currently empty"
   else 
     katz_deli.each do |i|
       string = 'The line is currently:'
