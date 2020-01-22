78c4e462d1cea343822bbcde1e493cdbfa4f7759
Kelly Powers <kpowers.nj@gmail.com>
2020-01-22 01:59:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@
 katz_deli = []
 def take_a_number(katz_deli, name)
     katz_deli << name 
-    puts "Welcome #{name}, you are number #{katz_deli.length} in line."
+    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
   
   
