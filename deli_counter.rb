d6f1ff3c2a4e577861e73673a35e38461c6fefcd
Kelly Powers <kpowers.nj@gmail.com>
2020-01-22 01:42:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,12 @@
 # Write your code here.
 katz_deli = []
+def take_a_number(katz_deli, name)
+  puts "Welcome #{name}, you are number #{katz_deli.length} in line."
+end
+  
+  
 def line(katz_deli)
-  num_ppl_in_line = 1
-  while katz_deli.length > 0 do 
-    puts "You are #{i}"
+  katz_deli.each do |i|
+    puts "#{i}. #{katz_deli[i]}"
+  end
+end
